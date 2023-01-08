0439a50a82a47ce78bdf8953b552be844f799db2
Tai <taiu.engineer@gmail.com>
2023-01-08 23:59:22 +0900

    dev thumbnail

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import Header from "../components/Header";
 import Banner from "../components/Banner";
 import requests from "../utils/requests";
 import { Movie } from "../typings";
+import Row from "../components/Row";
 
 interface Props {
   netflixOriginals: Movie[];
@@ -41,8 +42,16 @@ export default function Home({
       <Header />
       <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
         <Banner netflixOriginals={netflixOriginals} />
-        {/* <Banner /> */}
-        <section className="md:space-y-20"></section>
+        <section className="md:space-y-20">
+          <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
+          {/* My List Component */}
+          <Row title="Comedies" movies={comedyMovies} />
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
+        </section>
       </main>
       {/* Modal */}
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,5 +12,10 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-textshadow")],
+  plugins: [
+    require("tailwindcss-textshadow"),
+    require("tailwind-scrollbar-hide"),
+    require("tailwind-scrollbar"),
+    require("@tailwindcss/line-clamp"),
+  ],
 };
