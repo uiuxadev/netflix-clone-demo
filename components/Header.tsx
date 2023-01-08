ab6c7e38a87f38f4a77206c282068ec92f4cfa15
Tai <taiu.engineer@gmail.com>
2023-01-08 21:25:10 +0900

    dev header

---
+++ components/Header.tsx
@@ -0,0 +1,69 @@
+import Image from "next/image";
+import { BellIcon, SearchIcon } from "@heroicons/react/solid";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // if the display is scrolled, set "true"
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // clean up
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <header
+      className={`${isScrolled && "bg-[#141414] transition duration-300 "}`}
+    >
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <picture>
+          <img
+            src="https://rb.gy/ulxxee"
+            width={100}
+            height={100}
+            className="cursor-pointer object-contain"
+            alt="logo"
+          />
+        </picture>
+
+        <ul className="hidden space-x-4 md:flex">
+          <li className="headerLink">Home</li>
+          <li className="headerLink">TV Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My List</li>
+        </ul>
+      </div>
+
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <SearchIcon className="hidden h-6 w-6 sm:inline" />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="h-6 w-6" />
+        <Link href="/account">
+          <picture>
+            <img
+              src="https://rb.gy/g1pwyx"
+              alt=""
+              className="cursor-pointer rounded"
+            />
+          </picture>
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
