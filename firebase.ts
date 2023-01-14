d3dca9806b0322cfc6555a2b7117ca617424591f
Tai <taiu.engineer@gmail.com>
2023-01-14 14:18:33 +0900

    auth setting

--- firebase.ts
+++ firebase.ts
@@ -1,21 +1,27 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBWRSJg3xAYH6vA0hlSBVE2Ge5UDmQSVcE",
-  authDomain: "netflix-clone-demo-eda3d.firebaseapp.com",
-  projectId: "netflix-clone-demo-eda3d",
-  storageBucket: "netflix-clone-demo-eda3d.appspot.com",
-  messagingSenderId: "757234460708",
-  appId: "1:757234460708:web:c1bf33db54d5f8772b50b1",
-  measurementId: "G-78BTMFLR1C",
+  apiKey: "AIzaSyBuu0YHXOcgmqWLZl-fiMn6lG2wUhdPg8k",
+  authDomain: "next-firebase-stripe-39bf8.firebaseapp.com",
+  databaseURL: "https://next-firebase-stripe-39bf8-default-rtdb.firebaseio.com",
+  projectId: "next-firebase-stripe-39bf8",
+  storageBucket: "next-firebase-stripe-39bf8.appspot.com",
+  messagingSenderId: "777709922250",
+  appId: "1:777709922250:web:4500ee09dca93e1406d133",
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore();
+const auth = getAuth();
+
+export default app;
+export { auth, db };
